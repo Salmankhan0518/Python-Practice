59dd7aa482d3534a53d271ca926aab8f2443f8fa
Salmankhan0518 <salmansharif0518@gmail.com>
2026-04-20 01:15:11 +0500

    check fruit is ripe

---
+++ 02_conditionals/04_solution.py
@@ -0,0 +1,11 @@
+fruit = "Banana"
+color = "Yellow"
+
+if fruit == "Banana":
+    if color == "Yellow":
+        print("Ripe")
+    elif color == "Green":
+        print("Unripe")
+    elif color == "Brown":
+        print("Overripe")
+            
