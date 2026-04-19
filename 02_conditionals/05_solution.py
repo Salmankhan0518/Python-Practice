f65ac9e635e296788531d654948f994bea2e102f
Salmankhan0518 <salmansharif0518@gmail.com>
2026-04-20 01:26:13 +0500

    suggest weather acitivity

---
+++ 02_conditionals/05_solution.py
@@ -0,0 +1,10 @@
+weather = input("Enter the Weather then i'm suggest you Activity:")
+
+weather = weather.lower();
+
+if weather == "sunny":
+    print("Go for a walk")
+elif weather == "rainy":
+    print("Read a book")
+elif weather == "snowy":
+    print("Build a snowman")   
