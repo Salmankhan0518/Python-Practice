746a32bcd548ab973748586a139f931455fae01c
Salmankhan0518 <salmansharif0518@gmail.com>
2026-04-20 00:38:35 +0500

    01 solution age comperision

---
+++ 02_conditionals/01_solution.py
@@ -0,0 +1,12 @@
+age = int(input("Privide an age: "))
+
+if age < 13:
+    print("Child")
+elif age < 20:
+    print("Teenager")
+elif age < 60:
+    print("Adult")
+else:
+    print("Senior")
+
+    
