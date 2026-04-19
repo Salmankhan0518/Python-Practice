4f9976b7d0a6bac0c1943b566ff673a8a34525a2
Salmankhan0518 <salmansharif0518@gmail.com>
2026-04-20 01:03:36 +0500

    Giving Grade basis on marks

---
+++ 02_conditionals/03_solution.py
@@ -0,0 +1,12 @@
+marks = int( input("Enter your marks: ") )
+
+if marks >= 90 and marks <= 100:
+    print("Your Grade is A")
+elif marks >= 80 and marks <= 89:
+    print("Your Grade is B")
+elif marks >= 70 and marks <= 79:
+    print("Your Grade is C")
+elif marks >= 60 and marks <= 69:
+    print("Your Grade is D")
+else:
+    print("Fail")                
