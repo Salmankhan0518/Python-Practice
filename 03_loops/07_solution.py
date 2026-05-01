baf805e76e9b306167b640a1033b0cfdffd7ddf6
Salmankhan0518 <salmansharif0518@gmail.com>
2026-05-01 16:17:38 +0500

    enter number b/w 1 and 10

---
+++ 03_loops/07_solution.py
@@ -0,0 +1,9 @@
+while True:
+    number = int(input("Enter number b/w 1 and 10: "))
+    if 1<= number <= 10:
+        print(f"Thanks your number is {number}")
+        break
+    else:
+        print("Invalid Number! Please Enter again")
+
+
