fd800efb4d23ea06fc62dc5a9e36065697f326ea
Salmankhan0518 <salmansharif0518@gmail.com>
2026-05-23 15:45:26 +0500

    inheritence

---
+++ 07_oop/01_solution.py
@@ -0,0 +1,34 @@
+class Car:
+    def __init__(self, brand = "Honda", model = "Cevic"):
+        self.brand = brand
+        self.model = model
+
+    def full_name(self):
+        return f"{self.brand} {self.model}" 
+
+class ElectricCar(Car):
+    def __init__(self, brand, model, batterySize):
+        super().__init__(brand, model)
+        self.batterySize = batterySize
+
+
+my_tesla = ElectricCar("Tesla", "Model S", "85KWH")
+print(my_tesla)
+
+
+
+# my_car = Car("Toyota", "Carolla")
+
+# print(my_car.brand)
+# print(my_car.model)
+# print(my_car.full_name())
+
+# my_new_car = Car("Tata", "Safari")
+
+# print(my_new_car.brand)
+# print(my_new_car.model)
+
+# my_other_car = Car()
+
+# print(my_other_car.brand)
+# print(my_other_car.model)
